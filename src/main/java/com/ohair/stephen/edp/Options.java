1f488c426cb1408dafc15c39dadd848df963f565
Stephen O'Hair <stephen.ohair@gmail.com>
2018-05-05 17:31:41 +1000

    specify input/output default options

--- src/main/java/com/ohair/stephen/edp/Options.java
+++ src/main/java/com/ohair/stephen/edp/Options.java
@@ -14,16 +14,19 @@ import org.apache.beam.sdk.options.Validation;
 public interface Options extends PipelineOptions {
 	// Default to using a 1000 row subset of the public weather station table
 	// publicdata:samples.gsod.
-	static final String WEATHER_SAMPLES_TABLE = "clouddataflow-readonly:samples.weather_stations";
+	static final String GSOD_TABLE = "bigquery-public-data:noaa_gsod.gsod2017";
 
 	@Description("Table to read from, specified as " + "<project_id>:<dataset_id>.<table_id>")
-	@Default.String(WEATHER_SAMPLES_TABLE)
+	@Default.String(GSOD_TABLE)
 	String getInput();
 
 	void setInput(String value);
 
+	static final String PRECIPITATION_FILTERED_GSOD = "cloud-ai-weather-data-analyzer:precipitation.filtered_gsod";
+
 	@Description("BigQuery table to write to, specified as "
 			+ "<project_id>:<dataset_id>.<table_id>. The dataset must already exist.")
+	@Default.String(PRECIPITATION_FILTERED_GSOD)
 	@Validation.Required
 	String getOutput();
 
